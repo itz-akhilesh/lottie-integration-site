f27e7fa970b38d1fff2e85bc024c1e0244694029
itz-akhilesh <itzakhilesh@gmail.com>
2025-05-29 17:48:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
+// script.js — for JavaScript API method
 lottie.loadAnimation({
-  container: document.getElementById('lottie-animation'),
-  renderer: 'svg',
+  container: document.getElementById("lottie-animation"),
+  renderer: "svg",
   loop: true,
   autoplay: true,
-  path: 'assets/animation.json'
+  path: "assets/animation.json", // Local JSON file or URL
 });
